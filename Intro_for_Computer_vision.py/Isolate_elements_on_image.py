e25c61121e3fa352e9f8f6bb55226eb8a846ca1b
set <set>
2025-02-24 12:24:22 +0100

    Small changes

--- Intro_for_Computer_vision.py/Isolate_elements_on_image.py
+++ Intro_for_Computer_vision.py/Isolate_elements_on_image.py
@@ -3,7 +3,7 @@ import numpy
 from matplotlib import pyplot as plt
  
 
-image = cv2.imread(r"C:\Program Files (x86)\2 Semester python work\2-Semester-Computer-Vision\Intro_for_Computer_vision.py\fruits_hsv.jpg", cv2.IMREAD_GRAYSCALE)
+image = cv2.imread(r"Intro_for_Computer_vision.py\fruits_hsv.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Original Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
